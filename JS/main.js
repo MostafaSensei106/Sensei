6dd93e4dedb59bb14d776cccf737e92a4c9482f4
wwwmostafagmer100@gmail.com <wwwmostafagmer100@gmail.com>
2023-07-05 19:25:56 +0300

    v2.5 MH Web Page.

--- JS/main.js
+++ JS/main.js
@@ -1,4 +1,5 @@
 // THIS JAVA SCRIPT CODE BY MOSTAFA MAHMOUD
+
 var topBtn = document.getElementById("topBtn");
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function () {
@@ -13,16 +14,6 @@ function scrollFunction() {
     }
 }
 
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-    document.body.scrollTop = 0; // For Safari
-    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
-
-$(document).on('click', '.nav-link', function (e) {
-    $('a.active').removeClass('active');
-    $(this).addClass('active');
-})
 $(window).scroll(function () {
     var scrollDistance = $(window).scrollTop();
     $('.section').each(function (i) {
@@ -39,6 +30,17 @@ $(window).scroll(function () {
         }
     });
 }).scroll();
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+$(document).on('click', '.nav-link', function (e) {
+    $('a.active').removeClass('active');
+    $(this).addClass('active');
+})
 var header = document.getElementById("myHeader");
 $(document).on('click', '.form', function (e) {
     e.preventDefault();
@@ -76,4 +78,4 @@ function prevSlide() {
     showSlide(index - 1);
 }
 
-setInterval(nextSlide, 4000); // change slide every 3 seconds
+setInterval(nextSlide, 3000); // change slide every 3 seconds
