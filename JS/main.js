ddc3edcb06151499efde4c945ba69c37f759dca3
wwwmostafagmer100@gmail.com <wwwmostafagmer100@gmail.com>
2023-07-24 10:30:33 +0300

    v2.7 MH Web Page.

--- JS/main.js
+++ JS/main.js
@@ -11,11 +11,21 @@ window.onscroll = function () {
     scrollFunction()
 };
 
+
+// var dark = document.getElementById("dark-mode");
+// // When the user scrolls down 20px from the top of the document, show the button
+// window.onscroll = function () {
+//     scrollFunction()
+// };
+
+
 function scrollFunction() {
     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
         topBtn.style.display = "block";
+        // dark.style.display = "block"
     } else {
         topBtn.style.display = "none";
+        // dark.style.display = "none"
     }
 }
 
