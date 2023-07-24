2eb1f3df36059e8477a0c391786b5db8515555df
wwwmostafagmer100@gmail.com <wwwmostafagmer100@gmail.com>
2023-07-24 10:50:53 +0300

    v2.7 MH Web Page.

--- JS/main.js
+++ JS/main.js
@@ -11,25 +11,14 @@ window.onscroll = function () {
     scrollFunction()
 };
 
-
-// var dark = document.getElementById("dark-mode");
-// // When the user scrolls down 20px from the top of the document, show the button
-// window.onscroll = function () {
-//     scrollFunction()
-// };
-
-
 function scrollFunction() {
     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
         topBtn.style.display = "block";
-        // dark.style.display = "block"
     } else {
         topBtn.style.display = "none";
-        // dark.style.display = "none"
     }
 }
 
-// When the user clicks on the button, scroll to the top of the document
 function topFunction() {
     document.body.scrollTop = 0; // For Safari
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
