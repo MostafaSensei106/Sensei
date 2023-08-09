2fdd3636960e05467785b8dc4821306745be4e55
wwwmo <wwwmostafagmer100@gmail.com>
2023-08-09 21:37:24 +0300

    v3.7 MH Web Page.

--- JS/main.js
+++ JS/main.js
@@ -1,6 +1,4 @@
 // THIS JAVA SCRIPT CODE BY MOSTAFA MAHMOUD
-
-
 var page_loader = document.getElementById("page-loader");
 window.addEventListener("load", function () {
     page_loader.style.display = "none"
