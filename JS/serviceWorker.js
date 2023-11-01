6fb3bea0583e37bdfc79ebb9b265918bc6a6f2d9
MostafaSensei106 <wwwmostafagmer100@gmail.com>
2023-11-02 00:39:04 +0200

    v4.7 MH Web Page.

--- JS/serviceWorker.js
+++ JS/serviceWorker.js
@@ -1,55 +1,47 @@
-// Define the cache name
-const CACHE_NAME = "my-cache";
+// This is the "Offline page" service worker
 
-// Define an array of URLs to cache
-const CACHE_URLS = [
-    "../index.html",
-    "../css_files",
-    "lightbox.min.js",
-    "main.js",
-    "../images"
-];
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
-// Listen for the installation event
-self.addEventListener("install", event => {
-    // Wait until the promise resolves
-    event.waitUntil(
-        // Open the cache
-        caches.open(CACHE_NAME)
-            .then(cache => {
-                // Add all URLs to the cache
-                return cache.addAll(CACHE_URLS);
-            })
-    );
+const CACHE = "pwabuilder-page";
+
+// TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
+const offlineFallbackPage = "ToDo-replace-this-name.html";
+
+self.addEventListener("message", (event) => {
+  if (event.data && event.data.type === "SKIP_WAITING") {
+    self.skipWaiting();
+  }
 });
 
-// Listen for the fetch event
-self.addEventListener("fetch", event => {
-    // Respond with a custom response
-    event.respondWith(
-        // Check if there is a cached response for the request
-        caches.match(event.request)
-            .then(cachedResponse => {
-                // Return the cached response if there is one
-                if (cachedResponse) {
-                    return cachedResponse;
-                }
-                // Otherwise, make a network request and return the response
-                return fetch(event.request);
-            })
-    );
+self.addEventListener('install', async (event) => {
+  event.waitUntil(
+    caches.open(CACHE)
+      .then((cache) => cache.add(offlineFallbackPage))
+  );
 });
 
-// Listen for the activate event
-self.addEventListener("activate", event => {
-    // Delete any old caches that are not in use
-    event.waitUntil(
-        caches.keys()
-            .then(keys => {
-                return Promise.all(
-                    keys.filter(key => key !== CACHE_NAME)
-                        .map(key => caches.delete(key))
-                );
-            })
-    );
+if (workbox.navigationPreload.isSupported()) {
+  workbox.navigationPreload.enable();
+}
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.mode === 'navigate') {
+    event.respondWith((async () => {
+      try {
+        const preloadResp = await event.preloadResponse;
+
+        if (preloadResp) {
+          return preloadResp;
+        }
+
+        const networkResp = await fetch(event.request);
+        return networkResp;
+      } catch (error) {
+
+        const cache = await caches.open(CACHE);
+        const cachedResp = await cache.match(offlineFallbackPage);
+        return cachedResp;
+      }
+    })());
+  }
 });
