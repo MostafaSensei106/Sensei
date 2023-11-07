b6e3070df7104b5c990f355d95589009fedf4ef7
MostafaSensei106 <wwwmostafagmer100@gmail.com>
2023-11-07 22:56:39 +0200

    v4.7 MH Web Page.

--- JS/serviceWorker.js
+++ JS/serviceWorker.js
@@ -1,40 +1,29 @@
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
+const CACHE_NAME = "my-cache";
+// Define an array of URLs to cache
+const CACHE_URLS = ["../index.html", "../css_files", "lightbox.min.js", "main.js", "../images"];
 
-const CACHE = "my-cache";
-
-const offlineFallbackPage = "../index.html";
-
-self.addEventListener("message", (event) => {
-    if (event.data && event.data.type === "SKIP_WAITING") {
-        self.skipWaiting();
-    }
+// Listen for the installation event
+self.addEventListener("install", event => {
+    // Wait until the promise resolves
+    event.waitUntil(// Open the cache
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                // Add all URLs to the cache
+                return cache.addAll(CACHE_URLS);
+            }));
 });
 
-self.addEventListener('install', async (event) => {
-    event.waitUntil(caches.open(CACHE)
-        .then((cache) => cache.add(offlineFallbackPage)));
-});
-
-if (workbox.navigationPreload.isSupported()) {
-    workbox.navigationPreload.enable();
-}
-
-self.addEventListener('fetch', (event) => {
-    if (event.request.mode === 'navigate') {
-        event.respondWith((async () => {
-            try {
-                const preloadResp = await event.preloadResponse;
-
-                if (preloadResp) {
-                    return preloadResp;
+// Listen for the fetch event
+self.addEventListener("fetch", event => {
+    // Respond with a custom response
+    event.respondWith(// Check if there is a cached response for the request
+        caches.match(event.request)
+            .then(cachedResponse => {
+                // Return the cached response if there is one
+                if (cachedResponse) {
+                    return cachedResponse;
                 }
-
-                return await fetch(event.request);
-            } catch (error) {
-
-                const cache = await caches.open(CACHE);
-                return await cache.match(offlineFallbackPage);
-            }
-        })());
-    }
+                // Otherwise, make a network request and return the response
+                return fetch(event.request);
+            }));
 });
